ed4cd7e7701e2d0f3e64a79be920d2b43f5f3270
Qixia Lu <qixialu@hotmail.com>
2021-01-23 14:57:48 +1100

    React Redux Forms Revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import { Component } from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 // to make the state availible to main components
 const mapStateToProps = state => {
@@ -22,7 +23,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 })
 
 class Main extends Component {
@@ -75,7 +77,7 @@ render(){
       <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes}/>} />
       {/* the use of :dishId*/}
       <Route path = "/menu/:dishId" component={DishWithId}></Route>
-      <Route exact path='/contactus' component = {Contact}></Route>
+      <Route exact path='/contactus' component ={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />}/>
       <Redirect to="/home" />
     </Switch>
     {/* onclick is used to pass onDishSelect to menu componenet */}
